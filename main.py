85a8a66462c4a1e98c4e1c0cfe5246118b0f038d
ibrahim483/Seconderyschool_python2 <Ibrahim.abbalta@gmail.com>
2022-09-27 20:36:21 +0200

    How to find the moms from a price.

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
-print("ibrahim")
-print("ibbe")
+moms = 0.25
+price = input("What is your price? ")
+print("The price is ", price)
+price_moms = float(price)*moms
+print("The moms from the price will be", price_moms)
+rest = float(price)-price_moms
+print("The rest from the price will be", rest)
