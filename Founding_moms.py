0f3b73a706f6de5e21deaf17d35999a6f32a43be
ibrahim483/Seconderyschool_python2 <Ibrahim.abbalta@gmail.com>
2022-09-28 10:59:26 +0200

    How to find the moms from a price.

--- Founding_moms.py
+++ Founding_moms.py
@@ -5,4 +5,3 @@ price_moms = float(price)*moms
 print("The moms from the price will be", price_moms)
 rest = float(price)-price_moms
 print("The rest from the price will be", rest)
-print("Any more prices")
